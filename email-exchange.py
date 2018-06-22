8c40932166ee17fb220c7403d31c9a482accc8eb
wangyangyang <yanchu1990@hotmail.com>
2018-06-22 17:41:37 +0800

    0622

--- email-exchange.py
+++ email-exchange.py
@@ -3,31 +3,29 @@
 # Created on 2018/2/
 
 
-from exchangelib import DELEGATE, Account, Credentials, Configuration, NTLM, Message, Mailbox, HTMLBody
-from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
-
-
-#此句用来消除ssl证书错误，exchange使用自签证书需加上
+# from exchangelib import DELEGATE, Account, Credentials, Configuration, NTLM, Message, Mailbox, HTMLBody
+# from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
+import exchangelib
+# 此句用来消除ssl证书错误，exchange使用自签证书需加上
 BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
 
-
 # 输入你的域账号如example\leo
-cred = Credentials(r'EXAMPLE\leo', '输入你的密码')
+cred = exchangelib.Credentials(r'hywingroup\gjxx', 'hywin666')
 
-config = Configuration(server='输入邮箱服务器网页地址', credentials=cred, auth_type=NTLM)
-a = Account(
-    primary_smtp_address='输入你要绑定的邮箱名（leo@example.com)', config=config, autodiscover=False, access_type=DELEGATE
+config = exchangelib.Configuration(server='https://mail.hywingroup.com', credentials=cred, auth_type=exchangelib.NTLM)
+a = exchangelib.Account(
+    primary_smtp_address='gjxx@hywingroup.com', config=config, autodiscover=False, access_type=exchangelib.DELEGATE
 )
 
 # 此处为用来发送html格式邮件的文件路径
-with open(r'C:\Users\leo\Desktop\1.html') as f:
-    msg = f.read().decode('utf-8')
+# with open(r'D:\1.html') as f:
+#     msg = f.read().decode('utf-8')
 
-m = Message(
+m = exchangelib.Message(
     account=a,
     folder=a.sent,
     subject=u'测试邮件',
-    body=HTMLBody(msg),
-    to_recipients=[Mailbox(email_address='输入你要绑定的邮箱名（leo@example.com)')]
+    body='ceshiyoujian',
+    to_recipients=[Mailbox(email_address='wangyangyang@guinianggroup.com')]
 )
 m.send_and_save()
